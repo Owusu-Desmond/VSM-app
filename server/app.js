d27579b66f389dd4e2079a9afdc4449d856a7381
Owusu-Desmond <desmond0406114009@gmail.com>
2023-06-05 00:18:37 +0000

    Connection to database

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
+const env = require('dotenv')
 const api = require('./api')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+
+const app = express()
+env.config()
 
 app.set('port', (process.env.PORT || 8080))
 
@@ -17,10 +21,21 @@ app.use(express.static('static'))
 
 app.use(morgan('dev'))
 
-app.use((res, req) => {
+// Error handling middleware for 404 errors
+app.use((req, res) => {
   const err = new Error('Not found')
   err.status = 404
   res.json(err)
+})
+
+// Connect to database
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error: '))
+db.once('open', () => {
+  console.log('Connected to database')
 })
 
 app.listen(app.get('port'), () => {
