91386213ff93f449181a812a39480913c9572b61
Owusu-Desmond <desmond0406114009@gmail.com>
2025-05-17 22:24:00 +0000

    Add timeout to db connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,9 +2,21 @@ import config from "./config/env_config"
 import app from "./app";
 import { AppDataSource } from "./data-source";
 
+console.log("🟡 Starting server... Initializing DB connection");
+
+const CONNECTION_TIMEOUT = setTimeout(() => {
+  console.warn("⏳ Still waiting to connect to the database...");
+}, 5000);
+
 AppDataSource.initialize()
-.then(() => {
-        console.log("Database Connected")
-        app.listen(config.PORT, () => console.log(`🚀 Server running on port ${config.PORT}`))
-    }
-).catch(err => console.error('❌ DB connection failed', err))
+  .then(() => {
+    clearTimeout(CONNECTION_TIMEOUT);
+    console.log("Database Connected");
+    app.listen(config.PORT, () => {
+      console.log(`Server running on port ${config.PORT}`);
+    });
+  })
+  .catch((err) => {
+    clearTimeout(CONNECTION_TIMEOUT);
+    console.error("❌ DB connection failed", err);
+  });
